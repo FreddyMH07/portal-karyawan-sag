27871f90155f8a4030cf3bcb006919e61fb425e6
FreddyMH07 <FreddyMH07@users.noreply.github.com>
2025-06-23 12:05:46 +0000

    deploy: 167bbc5d11292fa529cd9f904d8eea09ef41e871

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     GOOGLE_SHEETS_ID: '1UMA4dHaqG6dmJ0kWTq7NPz6xmqnTPSqJpMu4ws7TFjs',
     
     // Google Apps Script Web App URL - UPDATED with correct URL
-    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbxRxM6Igt2CFOI84Ct9xhnJGjBRIGffwNeMk2KObj488KBToRLkG7D1e1BFjsVCTnTGaQ/exec',
+    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbzqMzUIMaMoUuPIEIAnfQHg5Ml6HNEp4S3eOo8CVTqZFi8vTqdIQt6o3iy3h5MViSqTlQ/exec',
     
     // Google OAuth Client ID (untuk Google Sign-In)
     GOOGLE_CLIENT_ID: '67c37218d77b709b2ac0d3aa5cdcc89e6e922dd0',
